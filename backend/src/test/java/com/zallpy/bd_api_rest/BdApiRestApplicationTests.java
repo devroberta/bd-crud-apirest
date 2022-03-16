212cb9edad0f256992e8458cbc131b7cb44b2a05
Roberta de Siqueira <designer.roberta@gmail.com>
2022-03-16 20:49:21 -0300

    Projeto spring initializr iniciado

---
+++ backend/src/test/java/com/zallpy/bd_api_rest/BdApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zallpy.bd_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
