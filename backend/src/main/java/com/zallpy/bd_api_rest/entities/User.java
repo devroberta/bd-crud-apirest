c62f4e8a6205b9a7b78a428846048f9cddc0fef4
Roberta de Siqueira <designer.roberta@gmail.com>
2022-03-19 21:43:19 -0300

    Testes no browser e no Postman para listar tabela, alterações nas classes e arquivos

--- backend/src/main/java/com/zallpy/bd_api_rest/entities/User.java
+++ backend/src/main/java/com/zallpy/bd_api_rest/entities/User.java
@@ -1,5 +1,8 @@
 package com.zallpy.bd_api_rest.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,12 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "table_user")
-public class User {
-
+@Table(name = "tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String nome;
 	private String sobrenome;
 	private String email;
@@ -22,7 +26,8 @@ public class User {
 	private User() {
 	}
 
-	public User(Integer id, String nome, String sobrenome, String email, Integer idade, Boolean ativo) {
+	public User(Long id, String nome, String sobrenome, String email, Integer idade, Boolean ativo) {
+		super();
 		this.id = id;
 		this.nome = nome;
 		this.sobrenome = sobrenome;
@@ -31,11 +36,11 @@ public class User {
 		this.ativo = ativo;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -77,5 +82,22 @@ public class User {
 
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 }
