67a9c5ed938db8599e2ab1ca2712e7db66d5a0b7
Roberta de Siqueira <designer.roberta@gmail.com>
2022-03-20 17:20:33 -0300

    Implementação CRUD

--- backend/src/main/java/com/zallpy/bd_api_rest/services/UserService.java
+++ backend/src/main/java/com/zallpy/bd_api_rest/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.zallpy.bd_api_rest.entities.User;
@@ -22,6 +23,23 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+			User entity = repository.findById(id).get();
+			entity.setNome(obj.getNome());
+			entity.setSobrenome(obj.getSobrenome());
+			entity.setEmail(obj.getEmail());
+			entity.setAtivo(obj.getAtivo());
+			return repository.save(entity);
 	}
 
 }
