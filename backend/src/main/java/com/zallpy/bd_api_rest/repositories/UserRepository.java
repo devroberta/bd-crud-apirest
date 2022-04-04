d71c928b23019fd9c3837b69357f49cf2fe1140b
matheusrocha <rengavmar1!S>
2022-04-04 17:13:01 -0300

    change folder

--- backend/src/main/java/com/zallpy/bd_api_rest/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zallpy.bd_api_rest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zallpy.bd_api_rest.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- backend/src/main/java/com/zallpy/bd_api_rest/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.zallpy.bd_api_rest.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public ResourceNotFoundException(Object id) {
-		super("ERRO: "+id);
-	}
-}
