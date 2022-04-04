19087deb26c81b6c97dfecfd1f926bd9e90c5caa
matheusrocha <rengavmar1!S>
2022-04-04 18:22:55 -0300

    made the code better

--- src/main/java/com/zallpy/bd_api_rest/entities/User.java
+++ src/main/java/com/zallpy/bd_api_rest/entities/User.java
@@ -1,36 +1,56 @@
 package com.zallpy.bd_api_rest.entities;
 
-import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.springframework.hateoas.RepresentationModel;
-
+import com.zallpy.bd_api_rest.dto.UserDTO;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_user")
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
-@Getter @Setter
+@Getter
+@Setter
 public class User extends RepresentationModel<User> implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	private String sobrenome;
-	private String email;
-	private Integer idade;
-	private Boolean ativo;
-	
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "nome")
+  private String nome;
+
+  @Column(name = "sobrenome")
+  private String sobrenome;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "idade")
+  private Integer idade;
+
+  @Column(name = "ativo")
+  private Boolean ativo;
+
+  public User(UserDTO dto) {
+    this.nome = dto.getNome();
+    this.sobrenome = dto.getSobrenome();
+    this.email = dto.getEmail();
+    this.idade = dto.getIdade();
+    this.ativo = dto.getAtivo();
+  }
 }
